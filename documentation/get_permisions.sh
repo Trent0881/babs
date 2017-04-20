d062997122ca3d3bd4762af0defb5fe1997b6b8d
Trent0881 <taz15@case.edu>
2017-04-20 09:49:48 -0400

    sw_integ.launch changes, and added a ROSbag of wobbler for sw itnegration testing

---
+++ documentation/get_permisions.sh
@@ -0,0 +1,10 @@
+sudo chmod 777 /dev/ttyACM0
+sudo chmod 777 /dev/ttyACM1
+
+sudo chmod 777 /dev/ttyUSB0
+sudo chmod 777 /dev/ttyUSB1
+sudo chmod 777 /dev/ttyUSB2
+sudo chmod 777 /dev/ttyUSB999 
+
+sudo chmod 777 /dev/video0
+sudo chmod 777 /dev/video1
